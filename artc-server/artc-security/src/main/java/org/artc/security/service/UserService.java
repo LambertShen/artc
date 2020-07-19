fdb84cf43a55389d2a4d154f173553b74e1fc53b
Lambert.shen@outlook.com <963963963Zz>
2020-07-19 23:14:35 +0800

    添加单元测试

---
+++ artc-server/artc-security/src/main/java/org/artc/security/service/UserService.java
@@ -0,0 +1,37 @@
+package org.artc.security.service;
+
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.artc.commom.utils.SnowFlake;
+import org.artc.security.entity.User;
+import org.artc.security.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private SnowFlake snowFlake;
+
+    public User findUserByLoginName(String loginName) {
+        return userMapper.findUserByLoginName(loginName);
+    }
+
+    public String insert(User user) {
+        String id = String.valueOf(snowFlake.nextId());
+        user.setId(id);
+        user.setCreated(LocalDateTime.now());
+        String salt = UUID.randomUUID().toString().replaceAll("-","");
+        SimpleHash simpleHash = new SimpleHash("MD5", user.getPassword(), salt, 1024);
+        user.setPassword(simpleHash.toHex());
+        user.setSalt(salt);
+        userMapper.insert(user);
+        return id;
+    }
+}
---
+++ artc-server/artc-web/src/main/test/java/org/artc/web/UserServiceTest.java
@@ -0,0 +1,34 @@
+package org.artc.web;
+
+import org.artc.security.entity.User;
+import org.artc.security.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void insert() {
+        User user = new User();
+        user.setLoginName("admin");
+        user.setEmail("2357111357@outlook.com");
+        user.setFamilyName("Shen");
+        user.setGiveName("Lambert");
+        user.setSex(0);
+        user.setHeadImgUrl("");
+        user.setIdCard("232700199809166317");
+        user.setNickName("Hacker");
+        user.setMobile("13634339865");
+        user.setPassword("963963");
+        userService.insert(user);
+    }
+
+}
